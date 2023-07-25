6072f149626a0179fd158362ff73027c5b18e687
pcw5092 <pcw5092@gmail.com>
2023-07-25 11:34:27 +0900

    feat:초기설정

---
+++ src/main/java/org/Instrgram/AppInstrgramApplication.java
@@ -0,0 +1,13 @@
+package org.Instrgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInstrgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInstrgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/Instrgram/AppInstrgramApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Instrgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInstrgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
